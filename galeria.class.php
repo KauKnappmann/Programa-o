ad90126566f027ef8a267bb747f679f53f57f1a7
kananak <kauanak21@gmail.com>
2020-06-08 17:35:14 -0300

    TASK00005 atributos inseridos
    galeria

--- galeria.class.php
+++ galeria.class.php
@@ -1,6 +1,22 @@
 <?php
 
   class Galeria_Fotos{
-    
+    //Atributos - variáveis
+    var $Codigo;
+    var $NomeImg;
+    var $DescricaoImg;
+    var $CaminhoImg;
+
+
+    //Métodos - funções
+    function UploadImg(){
+
+    }
+    function DeleteImg(){
+
+    }
+    function UploadImg(){
+
+    }
   }
 ?>
