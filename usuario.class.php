f00ad2db68ef8959e8e5deb3d744aaf95aee8032
Mary <52547463+maariaceciliaholler@users.noreply.github.com>
2020-06-08 15:47:19 -0300

    TASK00002 Criando Class usuario

---
+++ usuario.class.php
@@ -0,0 +1,20 @@
+<?php
+
+  class Usuario{
+    //Atributos - Variáveis
+    var $Nome;
+    var $Idade;
+    var $Ranking;
+
+    //Métodos - Funções
+
+    function IniciaJogo(){
+      //faz alfo
+    }
+
+    function AvaliaAcertos()
+
+  }
+
+
+?>
